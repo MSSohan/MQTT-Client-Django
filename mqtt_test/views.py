9d3069b3e654e25054176525733df7c2ae640fef
MSSohan <sohan.cu.eee.17@gmail.com>
2024-07-09 15:23:33 +0600

    initial files

---
+++ mqtt_test/views.py
@@ -0,0 +1,12 @@
+import json
+
+from django.http import JsonResponse
+
+from mqtt_test.mqtt import client as mqtt_client
+
+
+def publish_message(request):
+    request_data = json.loads(request.body)
+    # print(request_data)
+    rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
+    return JsonResponse({'code': rc})
